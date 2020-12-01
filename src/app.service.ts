ea3eef8e2ad127d1e6febc2413445f1b733a39dd
Mykhaylo Zeykan <mishkozejkan@gmail.com>
2020-12-01 22:05:07 +0200

    new test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'autodeploy completed, start postgres instalation';
+    return 'start postgres instalation bitch';
   }
 }
