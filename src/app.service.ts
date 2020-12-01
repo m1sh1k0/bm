dda40c7a66a76a2c9e3fe1cfe6846baa690aabd4
Mykhaylo Zeykan <mishkozejkan@gmail.com>
2020-12-01 22:17:16 +0200

    gospodi spasibo

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'start postgres instalation bitch, autodeploy works fine';
+    return 'start postgres instalation bitch, autodeploy works fine !!!!!';
   }
 }
