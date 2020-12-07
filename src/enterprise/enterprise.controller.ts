d8e4e69464aafc3856503de5ea560437274c47cb
Mykhaylo Zeykan <mishkozejkan@gmail.com>
2020-12-07 14:18:38 +0200

    added test docs

--- src/enterprise/enterprise.controller.ts
+++ src/enterprise/enterprise.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+  UploadedFile,
+  UseInterceptors,
+  Response,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+  ApiTags,
+  ApiConsumes,
+} from '@nestjs/swagger';
+@ApiTags('Enterprise')
+@ApiBearerAuth()
 @Controller('enterprise')
-export class EnterpriseController {}
+export class EnterpriseController {
+  @Get('/:id')
+  @ApiParam({ name: 'id', type: 'number' })
+  view(@Param() id: string) {
+    return id;
+  }
+
+  @Get()
+  list() {
+    return 'all enterprises';
+  }
+}
--- src/point/point.controller.ts
+++ src/point/point.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+  UploadedFile,
+  UseInterceptors,
+  Response,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+  ApiTags,
+  ApiConsumes,
+} from '@nestjs/swagger';
 
+@ApiTags('Point')
+@ApiBearerAuth()
 @Controller('point')
-export class PointController {}
+export class PointController {
+  @Get()
+  list() {
+    return 'all points of enterprise';
+  }
+}
