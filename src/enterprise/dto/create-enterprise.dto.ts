35bdcf26ddd00d2c900e58661e95c6377f5b3e5f
Mykhaylo Zeykan <mishkozejkan@gmail.com>
2020-12-08 23:17:14 +0200

    added enterprise dto

--- src/enterprise/dto/create-enterprise.dto.ts
+++ src/enterprise/dto/create-enterprise.dto.ts
@@ -1,7 +1,23 @@
 import { IsString } from 'class-validator';
-import {} from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateEnterpriseDto {
+  @ApiProperty()
   @IsString()
-  name: string;
+  name?: string;
+
+  @ApiProperty()
+  @IsString()
+  description?: string;
+
+  @ApiProperty()
+  @IsString()
+  adress?: string;
+
+  @ApiProperty()
+  @IsString()
+  image?: string;
+
+  users?: [];
+  stores?: [];
 }
--- src/enterprise/dto/update-enterprise.dto.ts
+++ src/enterprise/dto/update-enterprise.dto.ts
@@ -1,6 +1,25 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEnterpriseDto } from './create-enterprise.dto';
+import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateEnterpriseDto extends PartialType(CreateEnterpriseDto) {
+  @ApiProperty()
+  @IsString()
   name?: string;
+
+  @ApiProperty()
+  @IsString()
+  description?: string;
+
+  @ApiProperty()
+  @IsString()
+  adress?: string;
+
+  @ApiProperty()
+  @IsString()
+  image?: string;
+
+  users?: [];
+  stores?: [];
 }
